172bc40a5a781e9dc1f94256aec3dced0b2178ab
eihab-ahmed <eihab.ahmed@boniantech.com>
2023-05-19 06:42:59 +0300

    Complete game mechanics

---
+++ core/src/com/mygdx/rhythmtapperch06/Message.java
@@ -0,0 +1,36 @@
+package com.mygdx.rhythmtapperch06;
+
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.scenes.scene2d.Action;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
+
+public class Message extends BaseActor {
+    public Animation perfect;
+    public Animation great;
+    public Animation good;
+    public Animation almost;
+    public Animation miss;
+
+    public Message(float x, float y, Stage s) {
+        super(x, y, s);
+
+        perfect = loadTexture("perfect.png");
+        great = loadTexture("great.png");
+        good = loadTexture("good.png");
+        almost = loadTexture("almost.png");
+        miss = loadTexture("miss.png");
+    }
+
+    public void pulseFade() {
+        setOpacity(1);
+        clearActions();
+        Action pulseFade = Actions.sequence(
+                Actions.scaleTo(1.1f, 1.1f, 0.05f),
+                Actions.scaleTo(1.0f, 1.0f, 0.05f),
+                Actions.delay(1),
+                Actions.fadeOut(0.5f)
+        );
+        addAction(pulseFade);
+    }
+}
--- core/src/com/mygdx/rhythmtapperch06/RhythmTapperGame.java
+++ core/src/com/mygdx/rhythmtapperch06/RhythmTapperGame.java
@@ -5,27 +5,10 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-public class RhythmTapperGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+public class RhythmTapperGame extends BaseGame {
 	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
-
-	@Override
-	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+	public void create() {
+		super.create();
+		setActiveScreen(new RhythmScreen());
 	}
 }
